dd0534b473fe4653e5799d5bc72514e2eb38c422
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-06-03 16:00:50 +0000

    Added RST updated version

---
+++ constants.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# Created By: Aaron Rivelino
+# Date: Jun-02, 2025
+# Constants file
+
+# Pybadge screen is 160 x 28 and sprites are 16 x 16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
